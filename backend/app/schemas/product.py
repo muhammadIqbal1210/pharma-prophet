357844d6951a38b76d82fafe0e4305229331badd
muhammadIqbal1210 <iqbalmuhammad07015@gmail.com>
2026-05-15 23:29:43 +0700

    fix:perbaiki type data harga di produk

--- backend/app/schemas/product.py
+++ backend/app/schemas/product.py
@@ -1,11 +1,11 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 from typing import Optional, Literal
-
+from decimal import Decimal
 class ProductBase(BaseModel):
     nama_produk: str
     kategori: Literal["Obat", "Alkes"]
     satuan: Literal["Tablet", "Botol", "Strip", "Pcs"]
-    harga_jual: float
+    harga_jual: Decimal = Field(..., gt=0)  # Harga jual harus lebih besar dari 0
     stok_saat_ini: int
     stok_minimum: int
     deskripsi: Optional[str] = None
@@ -17,7 +17,7 @@ class ProductUpdate(BaseModel):
     nama_produk: Optional[str] = None
     kategori: Optional[str] = None
     satuan: Optional[str] = None
-    harga_jual: Optional[float] = None
+    harga_jual: Optional[Decimal] = None
     stok_saat_ini: Optional[int] = None
     stok_minimum: Optional[int] = None
     deskripsi: Optional[str] = None
