0c6b193ac02b90c2eaf58a29fa7cdb5bd9f879f6
muhammadIqbal1210 <iqbalmuhammad07015@gmail.com>
2026-06-18 09:29:09 +0700

    feat:tambah halaman prediksi

---
+++ frontend/app/pages/admin/prediksi/index.vue
@@ -0,0 +1,17 @@
+<script setup>
+definePageMeta({
+  layout: 'sidebar', // Menggunakan layouts/sidebar.vue
+  composables: 'useAuth'    // Menggunakan middleware/auth.ts
+})
+
+</script>
+<template>
+    <div class="space-y-6">
+        <div class="flex justify-between items-center">
+        <div>
+            <h1 class="text-2xl font-bold text-slate-900">Manajemen Prediksi</h1>
+            <p class="text-sm text-slate-500">Kelola dan pantau data prediksi penjualan obat apotek.</p>
+        </div>  
+    </div>
+    </div>    
+</template>
